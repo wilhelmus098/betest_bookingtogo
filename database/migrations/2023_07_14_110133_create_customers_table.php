674baaba0037d71d30fe478cab23d74c016c8140
wilhelm mozes <wilhelmmozes@gmail.com>
2023-07-14 18:37:53 +0700

    feat: add migration, create models

---
+++ database/migrations/2023_07_14_110133_create_customers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('country_id')->unsigned();
+            $table->string('name', 50);
+            $table->dateTime('dob');
+            $table->string('phone', 20);
+            $table->string('email', 50);
+
+            //define fk
+            $table->foreign('country_id')->references('id')->on('countries')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
---
+++ database/migrations/2023_07_14_110938_create_families_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFamiliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('families', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('customer_id')->unsigned();
+            $table->string('relation', 50);
+            $table->string('name', 50);
+            $table->string('dob', 50);
+
+            //define fk
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('families');
+    }
+}
